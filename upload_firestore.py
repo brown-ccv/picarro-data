44709a63c5db0e585c4ee4b2a2c9d28c25d10269
s-bessey <sam_bessey@brown.edu>
2024-07-29 13:19:33 -0400

    refactor: upload data in slices

--- upload_firestore.py
+++ upload_firestore.py
@@ -1,13 +1,28 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from collections import namedtuple
 
-def upload_df(data):
-# initialize sdk
+
+def upload_df(data, filename):
+    # initialize sdk
     cred = credentials.Certificate("serviceAccount.json")
     firebase_admin.initialize_app(cred)
 
     # initialize firestore instance
     db = firestore.client()
 
-    db.collection('col').document('doc').set(data.to_pandas().to_dict(orient='list')) # : google.api_core.exceptions.InvalidArgument: 400 too many index entries for entity
+    for i, frame in enumerate(data.iter_slices(n_rows=2000)):
+        year, month, day = frame["DATE"][0].split("-")
+        # datadict = dict(
+        #         [
+        #             (str(d["datetime"]), d)
+        #             for d in frame.drop(["DATE", "TIME"]).to_pandas().to_dict(orient="records")
+        #         ]
+        #     )
+        datadict = (
+            frame.drop(["DATE", "TIME"]).to_pandas().to_dict(orient="list")
+        )  # WORKS, don't love the data display
+        db.collection("picarro").document(year).collection(month).document(day).collection(f"{filename}").document(f"data_{i}").set(datadict)
 
+
+# google.api_core.exceptions.InvalidArgument: 400 Document 'projects/hastings-picarro/databases/(default)/documents/col/doc' cannot be written because its size (1,780,951 bytes) exceeds the maximum allowed size of 1,048,576 bytes.
