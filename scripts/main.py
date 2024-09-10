c5eb76065ea6228e3917f051692c99a180afc599
Yuan Ren <yren@picarro.com>
2024-09-10 16:15:16 -0400

    scheduled tasks working

--- scripts/main.py
+++ scripts/main.py
@@ -5,6 +5,8 @@ import upload_firestore
 import convert_dat
 import argparse
 import datetime
+from pathlib import WindowsPath
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("directory", help="Directory path")
@@ -12,10 +14,12 @@ parser.add_argument("--date", help="Date in YYYY-MM-DD format")
 parser.add_argument("--archive", action="store_true")
 args = parser.parse_args()
 
+sys.stdout=open(WindowsPath("C:/Users", "picarro", "Documents", "picarro-data", "logs", "log.txt"), 'w')
+
 if args.date:
     date = datetime.date.fromisoformat(args.date)
-else:  # if no date provided, use today's date
-    date = datetime.date.today()
+else:  # if no date provided, use yesterday's date
+    date = datetime.date.today() - datetime.timedelta(days=1)
 
 df = upload_storage.upload_data(args.directory, date, args.archive)
 
---
+++ scripts/run_main.ps1
@@ -0,0 +1,3 @@
+cd C:\Users\picarro\Documents\picarro-data\
+
+poetry run python -u C:\Users\picarro\Documents\picarro-data\scripts\main.py C:\Picarro\G2000\Log\DataLogger\DataLog_User
