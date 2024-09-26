8652f45bf862efb482fff8951c08b9869dc341ce
s-bessey <sam_bessey@brown.edu>
2024-09-26 15:34:29 -0400

    feat: allow start and end date for downloads

--- scripts/download_firestore.py
+++ scripts/download_firestore.py
@@ -1,6 +1,7 @@
 """Allows download of data from Firestore."""
 
 import argparse
+import datetime
 
 import pandas as pd
 import firebase_admin  # type: ignore
@@ -8,13 +9,18 @@ from firebase_admin import credentials, firestore
 
 parser = argparse.ArgumentParser()
 parser.add_argument("date", help="Date in YYYY-MM-DD format")
+parser.add_argument("--end", help="End date in YYY-MM-DD format (not inclusive)")
 parser.add_argument(
     "-f", "--filepath", help="Where to store the downloaded data", default="data/"
 )
 args = parser.parse_args()
 
-year, month, day = args.date.split("-")
+if args.end:
+    dates = pd.date_range(start=args.date, end=args.end, freq="ME")
+else:
+    dates = datetime.date.fromisoformat(args.date)
 
+print(dates)
 # initialize sdk
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred)
@@ -22,14 +28,17 @@ firebase_admin.initialize_app(cred)
 # initialize firestore instance
 db = firestore.client()
 
-collection = db.collection("picarro").document(year).collection(month)
+all_days = []
+for date in dates:
+    collection = db.collection("picarro").document(f"{date.year}").collection(f"{date.month:02}")
 
-docs = collection.stream()  # get all of the data in that collection
+    docs = collection.stream()  # get all of the data in that collection
 
-dfs = []
-for doc in docs:
-    dfs.append(doc.to_dict())
+    dfs = []
+    for doc in docs:
+        dfs.append(doc.to_dict())
+    print(dfs)
 
-df = pd.DataFrame.from_records(dfs)
+    all_days.append(pd.DataFrame.from_records(dfs))
 
-df.to_csv(f"{args.filepath}/data_{args.date}.csv", index=False)
+pd.concat(all_days).to_csv(f"{args.filepath}/data_{args.date}.csv", index=False)
