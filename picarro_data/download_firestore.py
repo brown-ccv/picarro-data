2f9d77ef240bdcb06bf572fd8d679009290528bd
s-bessey <sam_bessey@brown.edu>
2024-10-09 12:51:43 -0400

    update scripts

--- picarro_data/download_firestore.py
+++ picarro_data/download_firestore.py
@@ -10,25 +10,30 @@ parser.add_argument("date", help="Date in YYYY-MM-DD format")
 parser.add_argument(
     "-f", "--filepath", help="Where to store the downloaded data", default="data/"
 )
-args = parser.parse_args()
 
-year, month, day = args.date.split("-")
+def main():
+    args = parser.parse_args()
 
-# initialize sdk
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred)
+    year, month, day = args.date.split("-")
 
-# initialize firestore instance
-db = firestore.client()
+    # initialize sdk
+    cred = credentials.ApplicationDefault()
+    firebase_admin.initialize_app(cred)
 
-collection = db.collection("picarro").document(year).collection(month)
+    # initialize firestore instance
+    db = firestore.client()
 
-docs = collection.stream()  # get all of the data in that collection
+    collection = db.collection("picarro").document(year).collection(month)
 
-dfs = []
-for doc in docs:
-    dfs.append(doc.to_dict())
+    docs = collection.stream()  # get all of the data in that collection
 
-df = pd.DataFrame.from_records(dfs)
+    dfs = []
+    for doc in docs:
+        dfs.append(doc.to_dict())
 
-df.to_csv(f"{args.filepath}/data_{args.date}.csv", index=False)
+    df = pd.DataFrame.from_records(dfs)
+
+    df.to_csv(f"{args.filepath}/data_{args.date}.csv", index=False)
+
+if __name__ == "__main__":
+    main()
