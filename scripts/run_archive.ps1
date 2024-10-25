7f2fd6afccab8461455583642f45c293b5a9443d
Sam Bessey <sam_bessey@brown.edu>
2024-10-25 09:07:07 -0400

    Download scripts (#4)
    * Download scripts
    * Instructions for downloading

--- scripts/run_archive.ps1
+++
@@ -1,3 +0,0 @@
-cd C:\Users\picarro\Documents\picarro-data\
-
-poetry run python -u C:\Users\picarro\Documents\picarro-data\scripts\main.py C:\UserData\DataLog_User\2023\10\05 --date 2023-10-05 > C:\Users\picarro\Documents\picarro-data\logs\out.txt
