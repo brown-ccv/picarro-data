edbd3ee2650929dfecbb2436350e0116956b3822
tdivoll <tiothy_divoll@brown.edu>
2026-02-05 15:05:21 -0500

    fix: update typos in paths

--- picarro_data/scripts/run_main.cmd
+++ picarro_data/scripts/run_main.cmd
@@ -1,5 +1,5 @@
 REM Upload to Cloud Storage
-PowerShell poetry run python C:\Users\picarro\Documents\picarro-data\picarro-data\scripts\main_storage.py C:\Picarro\G2000\Log\DataLogger\DataLog_Users
+PowerShell poetry run python C:\Users\picarro\Documents\picarro-data\picarro_data\scripts\main_storage.py C:\Picarro\G2000\Log\DataLogger\DataLog_Users
 
 REM Upload to Firestore
-PowerShell poetry run python C:\Users\picarro\Documents\picarro-data\picarro-data\scripts\main_firestore.py C:\Picarro\G2000\Log\DataLogger\DataLog_Users
+PowerShell poetry run python C:\Users\picarro\Documents\picarro-data\picarro_data\scripts\main_firestore.py C:\Picarro\G2000\Log\DataLogger\DataLog_Users
