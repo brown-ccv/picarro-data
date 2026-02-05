3d5a16001ba2a2abe61d0565ba5d11d6dd3be425
tdivoll <tiothy_divoll@brown.edu>
2026-02-05 15:54:37 -0500

    fix: update paths in run scripts after live testing

--- picarro_data/scripts/run_main.ps1
+++ picarro_data/scripts/run_main.ps1
@@ -4,4 +4,4 @@ cd C:\Users\picarro\Documents\picarro-data\
 poetry run python -u C:\Users\picarro\Documents\picarro-data\picarro_data\scripts\main_storage.py C:\Picarro\G2000\Log\DataLogger\DataLog_User > C:\Users\picarro\Documents\picarro-data\logs\out.txt
 
 # Upload to Firestore
-poetry run python -u C:\Users\picarro\Documents\picarro-data\picarro_data\scripts\main_firestore.py C:\Picarro\G2000\Log\DataLogger\DataLog_User >> C:\Users\picarro\Documents\picarro-data\logs\out.txt
+poetry run python -u C:\Users\picarro\Documents\picarro-data\picarro_data\scripts\main_firestore.py C:\Picarro\G2000\Log\DataLogger\DataLog_User > C:\Users\picarro\Documents\picarro-data\logs\out.txt
